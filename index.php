3118566be7bc40ad0d0fc09a53bef80e40f3ffa7
63070026 <73680622+63070026@users.noreply.github.com>
2020-10-30 16:19:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php
-echo"<h2>PHP is fun!</h2>";
-echo"Hello world!<br>";
-?>
+<html>
+    <meta charset="UTF-8
+    <head>
+        <title>Profile</title>
+    </head>
+    <body>
+        <h1>My Profile</h1>
+        <p>My name is Chayanon Pettavee nickname is Nikk</p>
+        <img src="gecko-1.jpg">
+        <img src="codomo.jpg">
+        <img src="dog.jpg">
+        <br>
+        <a href="https://www.youtube.com/channel/UCI3GAvwaZwf1abbeREza8eA">I like this link </a>
+
+        <h2>คิดเรื่องตลกไม่ออก</h2>
+        
+
+    </body>
+</html>
